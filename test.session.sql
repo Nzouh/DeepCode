a33b3b2de69774be3b5e22cbb0b90e232a45e6ea
Nzouh <nzouh055@uottawa.ca>
2025-01-18 22:39:27 -0500

    adding database

--- test.session.sql
+++ test.session.sql
@@ -5,3 +5,12 @@ CREATE TABLE users (
     username VARCHAR(255) NOT NULL, 
     password VARCHAR(255) NOT NULL             -- Password (real or anonymized)
 );
+
+
+--@block
+
+SELECT * from users;
+
+--@block
+ALTER TABLE users
+ADD COLUMN protocol VARCHAR(5) AFTER url;
